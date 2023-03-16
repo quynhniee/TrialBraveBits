dc6ca8ea30c77753db03ef231c8100f32d8eb3a1
Nguyễn Như Quỳnh <nguyennquynh238@gmail.com>
2023-03-16 22:57:33 +0700

    fĩ calculator

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
